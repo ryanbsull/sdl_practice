aef9748cb1a45b902d9bcc86c814f1dce9f16437
Ryan B Sullivan <ryanbakersullivan@gmail.com>
2024-11-27 18:29:58 -0500

    sdl_practice: add in template file to later fill with a simple raycaster
    Need to practice creating 3D renders using a simple raycasting technique from the
    previous commit's link
    
    Signed-off-by: Ryan B Sullivan <ryanbakersullivan@gmail.com>

---
+++ src/raycast.c
@@ -0,0 +1,30 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#define SCREEN_WIDTH	640
+#define SCREEN_HEIGHT	400
+
+typedef uint32_t 	u32;
+typedef uint8_t 	u8;
+
+struct {
+	SDL_Window* win;
+	SDL_Renderer* renderer;
+	SDL_Texture* texture;
+	u32 pixels[SCREEN_WIDTH * SCREEN_HEIGHT];
+	u8 quit;
+} state;
+
+int init();
+int loop();
+int destroy();
+
+int main() {
+	if (init())
+		return 1;
+	while (!loop())
+		SDL_Delay(5);
+	destroy();
+	return 0;
+}
